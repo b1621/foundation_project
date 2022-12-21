1a7fe7650b38cc8938955ead78a3e0e18d2e187c
b <bewuket1621@gmail.com>
2022-12-21 18:32:00 +0300

    add tailwindcss to react app

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="containe border p-8 m-10">
       <h2>Hello world</h2>
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
     </div>
   );
 }

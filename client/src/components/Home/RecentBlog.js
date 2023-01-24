a09b5e5f4c92e848edead6f334b455f6c75d8c35
b <bewuket1621@gmail.com>
2023-01-24 21:14:26 +0300

    adding recent blog page

--- client/src/components/Home/RecentBlog.js
+++ client/src/components/Home/RecentBlog.js
@@ -1,14 +1,43 @@
 import React from 'react';
 
 function blog() {
+  const box_shadow =
+    'box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; color: red';
   return (
     <div className="border min-h-[500px]">
-      <div className="my-6 w-[700px] mx-auto font-serif">
+      <div className="my-6 w-[1000px] mx-auto font-serif">
         <h1 className="text-4xl text-center">Lattest Blogs</h1>
-        <div className="border flex ">
-          <div>blog 1</div>
-          <div>blog 2</div>
-          <div>blog 3</div>
+        <div className=" flex my-14">
+          <div className="border mx-3 w-[450px]">
+            <img src="images/eye.png" alt="" className="h-56 w-full" />
+            <p>August 12, 2018 | Jhon </p>
+            <h2>Symptoms of eye cancer</h2>
+            <p>
+              shadows, flashes of light, or wiggly lines in your vision. blurred
+              vision. a dark patch in your eye that's getting bigger. partial or
+              total loss of vision.
+            </p>
+          </div>
+          <div className="border mx-3 w-[450px]">
+            <img src="images/eye.png" alt="" className="h-56 w-full" />
+            <p>August 12, 2018 | Jhon </p>
+            <h2>Symptoms of eye cancer</h2>
+            <p>
+              shadows, flashes of light, or wiggly lines in your vision. blurred
+              vision. a dark patch in your eye that's getting bigger. partial or
+              total loss of vision.
+            </p>
+          </div>
+          <div className="border mx-3 w-[450px]" style={{ box_shadow }}>
+            <img src="images/eye.png" alt="" className="h-56 w-full" />
+            <p>August 12, 2018 | Jhon </p>
+            <h2>Symptoms of eye cancer</h2>
+            <p>
+              shadows, flashes of light, or wiggly lines in your vision. blurred
+              vision. a dark patch in your eye that's getting bigger. partial or
+              total loss of vision.
+            </p>
+          </div>
         </div>
       </div>
     </div>
